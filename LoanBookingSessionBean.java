53c160564904676b297150498efaee3edbd37443
“Manikandan2326” <manikandanshaw@gmail.com>
2022-08-18 17:12:38 +0000

    uk client-incident-codechanged-by -the develper-mani-on -19th -august

--- LoanBookingSessionBean.java
+++ LoanBookingSessionBean.java
@@ -1,12 +1,6 @@
 import java.util.Scanner;  
 import java.lang.Math;  
-import java.io.printer; 
-import java.io.collection; 
-import java.io.scanner;
-import java.io.outputprinter;
-import java.io.inputprinter;
-public class SpiralPatternExample2
-{  
+
 //function to print the spiral pattern  
 public static void printPattern(int n)  
 {  
